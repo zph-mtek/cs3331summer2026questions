330c4b4557f891a4f86d254393886cc79bc50527
Zack Horsch <zphorsch@mtu.edu>
2026-05-10 22:28:47 -0400

    updated

--- fork_zack.c
+++ fork_zack.c
@@ -3,19 +3,23 @@
 // Summer 2026
 // This program demonstrates the use of fork() to create a process tree. 
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
-void main() {
-    int pid = fork(); // Create a child process
-    printf("Process %d: First Split\n", getpid());
-    for (int i = 0; i < 3; i++) {
-        printf("Process %d: iteration %d\n", getpid(), i);
-        if (fork() == 0) { // This block is executed by the child process
+int main() {
+    printf("Parent PID = %d\n", getpid());
+    for (int i = 0; i < 2; i++) {
+        int pid = fork(); // Create a child process
+        if (pid == 0) { // This block is executed by the child process
+            fork();
+            wait(NULL);
             printf("Child process: PID = %d, Parent PID = %d\n", getpid(), getppid());
-            i++;
-        } else if (pid == 0) { // This block is executed by the parent process
+            exit(0); // Terminate the child process
+        } else { // This block is executed by the parent process
             printf("Parent process: PID = %d, Child PID = %d\n", getpid(), pid);
-            break;
+            wait(NULL); // Wait for the child process to finish
         }
     }
+    exit(0); // Terminate the parent process
 }
